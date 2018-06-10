d86491d71aaec829b14b1e836e12724976e863d9
pawelsuchocki <pawel.suchocki212@gmail.com>
2018-06-10 15:36:10 +0200

    add files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+     function changeImg1(){
+        document.getElementById('main-section').style.backgroundImage = "url(images/DLL.jpg)";
+      }
+      function changeImg2(){
+        document.getElementById('main-section').style.backgroundImage = "url(images/Benz.jpg)";
+      }
+      function changeImg3(){
+        document.getElementById('main-section').style.backgroundImage = "url(images/Cartoon.jpg)";
+      }
+
+
