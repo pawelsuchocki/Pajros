cd9c4962e320648ecc18de07a172aa3d9df21305
pawelsuchocki <pawel.suchocki212@gmail.com>
2018-06-14 09:22:34 +0200

    add scripts

--- js/script.js
+++ js/script.js
@@ -1,11 +1,43 @@
      function changeImg1(){
         document.getElementById('main-section').style.backgroundImage = "url(images/DLL.jpg)";
+        document.getElementById('main-section').style.transition = "background 1.0s linear 0s";
+        document.getElementById('main-section').style.background = clr;
       }
       function changeImg2(){
         document.getElementById('main-section').style.backgroundImage = "url(images/Benz.jpg)";
+        document.getElementById('main-section').style.transition = "background 1.0s linear 0s";
+        document.getElementById('main-section').style.background = clr;
       }
       function changeImg3(){
         document.getElementById('main-section').style.backgroundImage = "url(images/Cartoon.jpg)";
+        document.getElementById('main-section').style.transition = "background 1.0s linear 0s";
+        document.getElementById('main-section').style.background = clr;
+      }
+
+  function changeImg4(){
+        
+        document.getElementById('main-section').style.transition = "background 1.0s linear 0s";
+        document.getElementById('main-section').style.background = clr;
+      }
+      function changeImg5(){
+        
+        document.getElementById('main-section').style.transition = "background 1.0s linear 0s";
+        document.getElementById('main-section').style.background = clr;
+      }
+      function changeImg6(){
+        
+        document.getElementById('main-section').style.transition = "background 1.0s linear 0s";
+        document.getElementById('main-section').style.background = clr;
       }
 
 
+//var szerokosc = screen.width,
+ //   wysokosc = screen.height; 
+
+//var minSzerokosc = 768,
+//    minWysokosc = 768;
+
+//if (szerokosc =< minSzerokosc && wysokosc =< minWysokosc) {
+  //  document.getElementById('main-section').style.backgroundImage = "url(images/Cartoon.jpg)";
+//} else {
+//}
